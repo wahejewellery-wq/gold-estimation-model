55ce02c3415cb4bfb5ad4f5a344a55bae47b46a5
wahejewellery-wq <wahejewellery@gmail.com>
2026-02-18 20:17:06 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Gold & Diamond Estimation API")
 # Add CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend URL
+    allow_origins=["*"],  # Frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
